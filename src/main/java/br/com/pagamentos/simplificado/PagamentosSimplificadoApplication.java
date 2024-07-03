6b4b74835558027d7c2dcaca8c30d0f899bc7b5f
Felipe Couto <couto.felipe2@gmail.com>
2024-07-03 20:46:55 -0300

    Primeiro commit, criando projeto

---
+++ src/main/java/br/com/pagamentos/simplificado/PagamentosSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pagamentos.simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pagamentos/simplificado/PagamentosSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pagamentos.simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
